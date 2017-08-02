541aa741ffbb24d8f65760b4447595f59e518e4f
Samuel Thomas <sgpthomas@gmail.com>
2017-08-02 19:35:02 +0000

    removed pydsef as a gitmodule

--- wrapper.py
+++ wrapper.py
@@ -8,21 +8,15 @@ from time import sleep
 import sys
 from gen_experiments import main as gen
 import copy
-from pydsef import Service, experiment
+from pydsef import Service, experiment, Registry
 
 NUM_TRIES = 10
 
-# class WrapperService(rpyc.Service):
+@Registry.experiment
 class WrapperService(Service):
-    def on_connect(self):
-        pass
 
-    def on_disconnect(self):
-        pass
-
-    def exposed_setup(self, ed, hosts):
-        ed = copy.deepcopy(ed)
-        hosts = copy.deepcopy(hosts)
+    @Registry.setup
+    def setup(self, ed, hosts):
         self.run_process = subprocess.Popen(['./dsef/run.py'])
 
         self.conn = None
@@ -46,21 +40,18 @@ class WrapperService(Service):
         gen(ed, hosts)
         self.root.setup(ed, hosts)
 
-    def exposed_launch(self):
+    @Registry.launch
+    def launch(self):
         self.root.launch()
 
-    def exposed_run(self):
+    @Registry.run
+    def run(self):
         return self.root.run()
 
-    def exposed_teardown(self):
+    @Registry.teardown
+    def teardown(self):
         self.root.teardown()
 
         self.conn.close()
         self.root = None
         self.run_process.kill()
-
-if __name__ == "__main__":
-    from rpyc.utils.server import *
-    t = ThreadedServer(WrapperService, port = 18861, protocol_config = {'allow_pickle':True})
-    print("Starting RPyC Server...")
-    t.start()
